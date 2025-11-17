08d6cdac446da12ab014c74cf270721d56f3cf79
immortal521 <immort521@gmail.com>
2025-11-17 13:31:32 +0800

    style: update diagnostic error sign icon
    - Replace commented out icon with visible error sign symbol

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -145,7 +145,6 @@ vim.diagnostic.config({
   severity_sort = true,
   signs = {
     text = {
-      -- [vim.diagnostic.severity.ERROR] = '',
       [vim.diagnostic.severity.ERROR] = " ",
       [vim.diagnostic.severity.WARN] = " ",
       [vim.diagnostic.severity.INFO] = " ",
