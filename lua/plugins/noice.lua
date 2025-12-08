7372d82d78b98a9937a61deb35a8be366b43808f
immortal521 <immort521@gmail.com>
2025-12-08 20:10:11 +0800

    feat: disable LSP signature in noice plugin
    - Add option to disable signature display in LSP setup

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -4,6 +4,7 @@ vim.pack.add({
 
 require("noice").setup({
   lsp = {
+    signature = { enabled = false },
     override = {
       ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
       ["vim.lsp.util.stylize_markdown"] = true,
