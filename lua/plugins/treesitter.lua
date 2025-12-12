47585d91bd3f909f62b26f44ebe9f358981d1f1b
immortal521 <immort521@gmail.com>
2025-12-12 22:14:10 +0800

    chore: pin nvim-treesitter to main branch
    - Add new plugins blink-cmp-rg.nvim and colorful-menu.nvim to lockfile

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,5 @@
 vim.pack.add({
-  { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
   { src = "https://github.com/windwp/nvim-ts-autotag" },
   { src = "https://github.com/folke/ts-comments.nvim" },
 })
