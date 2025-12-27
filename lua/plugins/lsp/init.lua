0b521fde0b2f8f625197e9febf88b576ba1e3c7d
immortal521 <immort521@gmail.com>
2025-12-28 02:53:48 +0800

    refactor: move nvim-jdtls plugin declaration to lsp init file
    - Remove standalone nvim-jdtls plugin file
    
    - Add nvim-jdtls to lsp plugin list for centralized management

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -2,6 +2,7 @@ vim.pack.add({
   { src = "https://github.com/mfussenegger/nvim-lint" },
   { src = "https://github.com/mason-org/mason.nvim" },
   { src = "https://github.com/neovim/nvim-lspconfig" },
+  { src = "https://github.com/mfussenegger/nvim-jdtls" },
 })
 
 vim.api.nvim_create_autocmd(Utils.autocmd.BufEdit, {
