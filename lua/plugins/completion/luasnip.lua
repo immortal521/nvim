ae2763ded17a8444210ab0816626fef780ff12a6
immortal521 <immort521@gmail.com>
2025-12-29 11:43:31 +0800

    refactor: update luasnip setup configuration
    - Switch to using luasnip.config.setup
    
    - Remove history and ext_opts settings

--- lua/plugins/completion/luasnip.lua
+++ lua/plugins/completion/luasnip.lua
@@ -1,15 +1,7 @@
-require("luasnip").setup({
-  history = true,
+require("luasnip.config").setup({
   delete_check_events = "TextChanged",
   update_events = { "TextChanged", "TextChangedI" },
   enable_autosnippets = true,
-  ext_opts = {
-    [require("luasnip.util.types").choiceNode] = {
-      active = {
-        virt_text = { { " <- Choice ", "NonTest" } },
-      },
-    },
-  },
 })
 
 require("luasnip.loaders.from_vscode").lazy_load()
