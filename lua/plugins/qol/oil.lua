043c7a8e8ce5f34a20ad3b194d78751128bf9c62
immortal521 <immort521@gmail.com>
2025-12-31 22:52:21 +0800

    feat: add custom winbar for Oil plugin
    - Define global function to show current directory or buffer name
    
    - Set winbar option to use the custom function in Oil windows

--- lua/plugins/qol/oil.lua
+++ lua/plugins/qol/oil.lua
@@ -5,6 +5,14 @@ return {
 	cmd = "Oil",
 	lazy = false,
 	opts = function()
+		function _G.get_oil_winbar()
+			local dir = require("oil").get_current_dir()
+			if dir then
+				return vim.fn.fnamemodify(dir, ":~")
+			else
+				return vim.api.nvim_buf_get_name(0)
+			end
+		end
 		---@type oil.SetupOpts
 		return {
 			default_file_explorer = true,
@@ -43,6 +51,9 @@ return {
 			use_default_keymaps = false,
 			view_options = {
 				-- show_hidden = true,
+			},
+			win_options = {
+				winbar = "%!v:lua.get_oil_winbar()",
 			},
 		}
 	end,
