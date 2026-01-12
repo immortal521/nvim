a28f9e483163bbee080a03960dcb48ff428d6b1c
immortal521 <immort521@gmail.com>
2026-01-12 19:40:04 +0800

    feat: add oil-git.nvim plugin configuration
    - Enable file highlights and ignored files display
    
    - Disable directory highlights by default

---
+++ lua/plugins/qol/oil-git.lua
@@ -0,0 +1,10 @@
+---@type LazyPluginSpec
+return {
+	"malewicz1337/oil-git.nvim",
+	ft = "oil",
+	opts = {
+		show_file_highlights = true,
+		show_directory_highlights = false,
+		show_ignored_files = true,
+	},
+}
