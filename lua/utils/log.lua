948da19f823b6bba67f7683c26dae19dc5038285
immortal521 <immort521@gmail.com>
2026-02-02 15:19:19 +0800

    refactor: enhance logging module with optional timestamp and type annotations
    - Add LogOpts for optional timestamp display
    
    - Update log functions to accept options
    
    - Improve code formatting and add LuaDoc comments

--- lua/utils/log.lua
+++ lua/utils/log.lua
@@ -1,76 +1,92 @@
 ---@class utils.log
 local M = setmetatable({}, {
-  __call = function(m, ...)
-    return m.log(...)
-  end,
+	__call = function(m, ...)
+		return m.log(...)
+	end,
 })
 
 --- 日志级别
+---@type table<string, number>
 M.levels = {
-  DEBUG = 1,
-  INFO = 2,
-  WARN = 3,
-  ERROR = 4,
+	DEBUG = 1,
+	INFO = 2,
+	WARN = 3,
+	ERROR = 4,
 }
 
 --- 当前日志级别
 M.current_level = M.levels.INFO
 
 --- 设置日志级别
----@param level number 日志级别
-M.set_level = function(level)
-  M.current_level = level
+---@param level number
+function M.set_level(level)
+	M.current_level = level
 end
 
---- 带时间戳的日志输出
----@param msg string 要记录的消息
----@param level? number 日志级别，默认为 INFO
-M.log = function(msg, level)
-  level = level or M.levels.INFO
+---@class LogOpts
+---@field time? boolean 是否显示时间（默认 true）
 
-  if level < M.current_level then
-    return
-  end
+--- 日志输出
+---@param msg string
+---@param level? number
+---@param opts? LogOpts
+function M.log(msg, level, opts)
+	level = level or M.levels.INFO
+	opts = opts or {}
 
-  local time = os.date("%Y-%m-%d %H:%M:%S")
-  local level_name = level == M.levels.DEBUG and "DEBUG"
-    or level == M.levels.INFO and "INFO"
-    or level == M.levels.WARN and "WARN"
-    or level == M.levels.ERROR and "ERROR"
-    or "INFO"
+	if level < M.current_level then
+		return
+	end
 
-  local formatted_msg = ("[%s] %s - %s"):format(level_name, time, msg)
+	local show_time = opts.time ~= false
 
-  -- 使用 vim.notify 以获得更好的通知体验
-  local vim_level = level == M.levels.ERROR and vim.log.levels.ERROR
-    or level == M.levels.WARN and vim.log.levels.WARN
-    or vim.log.levels.INFO
+	local level_name = level == M.levels.DEBUG and "DEBUG"
+		or level == M.levels.INFO and "INFO"
+		or level == M.levels.WARN and "WARN"
+		or level == M.levels.ERROR and "ERROR"
+		or "INFO"
 
-  vim.notify(formatted_msg, vim_level)
+	local formatted_msg
+	if show_time then
+		local time = os.date("%Y-%m-%d %H:%M:%S")
+		formatted_msg = ("[%s] %s - %s"):format(level_name, time, msg)
+	else
+		formatted_msg = ("[%s] %s"):format(level_name, msg)
+	end
+
+	local vim_level = level == M.levels.ERROR and vim.log.levels.ERROR
+		or level == M.levels.WARN and vim.log.levels.WARN
+		or vim.log.levels.INFO
+
+	vim.notify(formatted_msg, vim_level)
 end
 
 --- 调试日志
----@param msg string 消息
-M.debug = function(msg)
-  M.log(msg, M.levels.DEBUG)
+---@param msg string
+---@param opts? LogOpts
+function M.debug(msg, opts)
+	M.log(msg, M.levels.DEBUG, opts)
 end
 
 --- 信息日志
----@param msg string 消息
-M.info = function(msg)
-  M.log(msg, M.levels.INFO)
+---@param msg string
+---@param opts? LogOpts
+function M.info(msg, opts)
+	M.log(msg, M.levels.INFO, opts)
 end
 
 --- 警告日志
----@param msg string 消息
-M.warn = function(msg)
-  M.log(msg, M.levels.WARN)
+---@param msg string
+---@param opts? LogOpts
+function M.warn(msg, opts)
+	M.log(msg, M.levels.WARN, opts)
 end
 
 --- 错误日志
----@param msg string 消息
-M.error = function(msg)
-  M.log(msg, M.levels.ERROR)
+---@param msg string
+---@param opts? LogOpts
+function M.error(msg, opts)
+	M.log(msg, M.levels.ERROR, opts)
 end
 
 return M
