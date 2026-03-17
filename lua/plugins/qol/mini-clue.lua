7754f4b5dbc897d438b88e2ea5d4a7bee910e58f
immortal521 <immort521@gmail.com>
2026-03-18 02:05:08 +0800

    feat: add leader key group clues for mini.clue plugin
    - Define descriptive leader key groups for normal and visual modes
    
    - Replace Config.leader_group_clues with local leader_group_clues variable

--- lua/plugins/qol/mini-clue.lua
+++ lua/plugins/qol/mini-clue.lua
@@ -1,3 +1,24 @@
+local leader_group_clues = {
+	{ keys = "<leader>a", mode = { "n", "x" }, desc = "+ai" },
+	{ keys = "<leader>b", mode = "n", desc = "+buffer" },
+	{ keys = "<leader>c", mode = { "n", "x" }, desc = "+code" },
+	{ keys = "<leader>cp", mode = { "n", "x" }, desc = "+code preview" },
+	{ keys = "<leader>d", mode = { "n", "x" }, desc = "+debug" },
+	{ keys = "<leader>f", mode = "n", desc = "+find" },
+	{ keys = "<leader>g", mode = "n", desc = "+git" },
+	{ keys = "<leader>l", mode = "n", desc = "+lsp" },
+	{ keys = "<leader>o", mode = "n", desc = "+overseer" },
+	{ keys = "<leader>q", mode = "n", desc = "+session" },
+	{ keys = "<leader>s", mode = { "n", "x" }, desc = "+search" },
+	{ keys = "<leader>t", mode = "n", desc = "+terminal" },
+	{ keys = "<leader>u", mode = "n", desc = "+ui" },
+	{ keys = "<leader>w", mode = "n", desc = "+window" },
+	{ keys = "<leader>x", mode = "n", desc = "+other" },
+	{ keys = "<leader>e", mode = "n", desc = "files" },
+	{ keys = "<leader>p", mode = "n", desc = "+pack" },
+	{ keys = "<leader><tab>", mode = "n", desc = "+tabs" },
+}
+
 ---@type LazyPluginSpec
 return {
 	"nvim-mini/mini.clue",
@@ -35,7 +56,7 @@ return {
 			},
 
 			clues = {
-				Config.leader_group_clues,
+				leader_group_clues,
 
 				-- Enhance this by adding descriptions for <Leader> mapping groups
 				clue.gen_clues.builtin_completion(),
