4855428185389a7e18eaab26022b02c98159a9f5
immortal521 <immort521@gmail.com>
2026-04-12 01:37:45 +0800

    style: remove automatic comment insertion on new lines
    - Disable 'c', 'r', and 'o' formatoptions for all filetypes to prevent auto comment continuation

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,6 +6,12 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 	end,
 })
 
+vim.api.nvim_create_autocmd("FileType", {
+	callback = function()
+		vim.opt_local.formatoptions:remove({ "c", "r", "o" })
+	end,
+})
+
 -- 处理所有特殊缓冲区类型
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = {
