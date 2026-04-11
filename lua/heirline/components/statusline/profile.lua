9b9ae7f9dccbeb8b0cda9bcf120b225133feac81
immortal521 <immort521@gmail.com>
2026-04-12 01:38:02 +0800

    refactor: comment out noice command status provider in profile component
    - Disable noice status command output in statusline profile.lua

--- lua/heirline/components/statusline/profile.lua
+++ lua/heirline/components/statusline/profile.lua
@@ -3,7 +3,7 @@ local colors = Utils.colors()
 return {
 	{
 		provider = function()
-			return require("noice").api.status.command.get()
+			-- return require("noice").api.status.command.get()
 		end,
 		hl = { fg = colors.magenta },
 	},
