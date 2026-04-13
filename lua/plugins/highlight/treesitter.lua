d8c037404ad0113a523eb6bbbe1b558fb05cf920
immortal521 <immort521@gmail.com>
2026-04-14 01:29:27 +0800

    feat: enhance treesitter setup with lazy parser installation and expanded language support
    - Add lazy plugin spec for treesitter-parser-registry
    
    - Expand supported languages list
    
    - Implement autocmd to auto-install missing parsers on file open
    
    - Add file size check to skip large files
    
    - Remove eager parser installation loop in config function

--- lua/plugins/highlight/treesitter.lua
+++ lua/plugins/highlight/treesitter.lua
@@ -1,48 +1,35 @@
+-- stylua: ignore
 local langs = {
-	"bash",
-	"c",
+	"bash", "bibtex",
+	"c", "cpp", "cmake",
+	"clojure",
 	"css",
-	"diff",
+	"diff", "dockerfile",
+	"elixir", "erlang",
+	"fish",
+	"git_config", "gitcommit", "gitattributes", "gitignore", "git_rebase",
+	"go", "gomod", "gowork", "gosum",
 	"graphql",
-	"html",
+	"haskell", "html",
 	"http",
-	"java",
-	"javascript",
-	"jsdoc",
-	"json",
-	"jsx",
-	"lua",
-	"luadoc",
-	"luap",
-	"markdown",
-	"markdown_inline",
-	"nu",
-	"printf",
-	"python",
+	"java", "javascript", "jsdoc", "json", "jsx",
+	"kotlin",
+	"latex", "lua", "luadoc", "luap",
+	"make", "markdown", "markdown_inline",
+	"nim", "ninja", "nu",
+	"ocaml",
+	"php", "proto", "python",
 	"query",
-	"go",
-	"gomod",
-	"gowork",
-	"gosum",
-	"git_config",
-	"gitcommit",
-	"git_rebase",
-	"gitignore",
-	"gitattributes",
-	"regex",
-	"rust",
-	"ron",
-	"scss",
-	"toml",
-	"tsx",
-	"typescript",
-	"vue",
-	"vim",
-	"vimdoc",
+	"regex", "ron", "ruby", "rust",
+	"scss", "sql", "swift",
+	"toml", "tsx", "typescript",
+	"vim", "vimdoc", "vue",
 	"xml",
 	"yaml",
+	"zig",
 }
 
+---@type LazyPluginSpec
 return {
 	"neovim-treesitter/nvim-treesitter",
 	branch = "main",
@@ -50,10 +37,41 @@ return {
 	event = { "BufEdit", "VeryLazy" },
 	opts = {},
 	init = function()
-		vim.api.nvim_create_autocmd({ "FileType" }, {
-			pattern = langs,
-			callback = function()
-				vim.treesitter.start()
+		vim.api.nvim_create_autocmd("FileType", {
+			group = vim.api.nvim_create_augroup("NativeTreesitter", { clear = true }),
+			callback = function(args)
+				local buf = args.buf
+				local ft = vim.bo[buf].filetype
+
+				if ft == "" or vim.bo[buf].buftype ~= "" then
+					return
+				end
+
+				local max_filesize = 100 * 1024 * 1024 -- 100 MB
+				local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
+				if ok and stats and stats.size > max_filesize then
+					return
+				end
+
+				local lang = vim.treesitter.language.get_lang(ft) or ft
+
+				if not vim.tbl_contains(langs, lang) then
+					return
+				end
+
+				local no_err, is_added = pcall(vim.treesitter.language.add, lang)
+				if not no_err or not is_added then
+					vim.notify("Installing " .. lang .. " parser...", vim.log.levels.INFO)
+					local ts = require("nvim-treesitter")
+					if ts.install then
+						ts.install({ lang }):wait(60000)
+					else
+						vim.cmd("TSInstall " .. lang)
+					end
+				end
+
+				pcall(vim.treesitter.start, buf, lang)
+
 				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
 				vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
 				vim.wo[0][0].foldmethod = "expr"
@@ -63,8 +81,5 @@ return {
 	config = function(_, opts)
 		local TS = require("nvim-treesitter")
 		TS.setup(opts)
-		for _, lang in ipairs(langs) do
-			TS.install(lang)
-		end
 	end,
 }
