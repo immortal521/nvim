9e96713ba2c1f47bce066d9edd59c62ea4bc1395
immortal521 <immort521@gmail.com>
2026-07-30 18:03:10 +0800

    fix: update window validity check in offset component
    - Use window config's zindex for condition
    
    - Replace buffer filetype check with window validity check

--- lua/heirline/components/tabline/offset.lua
+++ lua/heirline/components/tabline/offset.lua
@@ -9,7 +9,8 @@ return {
 
 		---@cast win integer
 		self.winid = win
-		return vim.bo[vim.api.nvim_win_get_buf(win)].filetype == "snacks_layout_box"
+		local config = vim.api.nvim_win_get_config(win)
+		return config.zindex ~= nil and not vim.api.nvim_win_is_valid(vim.api.nvim_get_current_win())
 	end,
 	provider = function(self)
 		return string.rep(" ", vim.api.nvim_win_get_width(self.winid)) .. self.separator
